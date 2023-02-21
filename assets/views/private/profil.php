aa9cad1d1384c6761b86ff600a4bcb56ff34e53b
Adrien Kouyoumjian <adrien.kouyoumjian@outlook.fr>
2023-02-21 15:21:17 +0100

    ajout des menu privées sur toutes les pages private

---
+++ assets/views/private/profil.php
@@ -0,0 +1,11 @@
+<h1 class="mt-5 text-center">Bienvenue sur votre tableau de bord</h1>
+<section class="row">
+    <div class="col-3 p-3">
+        <?php require "assets/lib/menu-privee.php" ?>
+    </div>
+    <div class="col">
+
+
+
+    </div>
+</section>
--- assets/views/public/login.php
+++ assets/views/public/login.php
@@ -12,18 +12,4 @@
             <input type="submit" class="btn btn-outline-primary">
         </div>
     </div>
-    <?php if(!empty($_SESSION["message"])) : ?>
-    <?php if($_SESSION["message"]["alert"] === "success") : ?> 
-        <p class="alert alert-success" >
-            <?php echo $_SESSION["message"]["info"]  ?>
-        </p>
-    <?php elseif($_SESSION["message"]["alert"] === "danger") : ?> 
-        <p class="alert alert-danger">
-            <?php foreach($_SESSION["message"]["info"] as $i) : ?>
-                <?php echo $i  ?><br>
-            <?php endforeach ?>
-        </p>
-    <?php endif ?>
-<?php endif ?>
-
 </form>
